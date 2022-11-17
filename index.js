c4c900eaa4144781991439a09ec1f0854654e906
Jose Andrés <oliver_ja@outlook.com>
2022-11-17 19:59:37 +0100

    script inicial mejorado

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const qrcode = require('qrcode')
 const {exec} = require("child_process");
 const fs = require('fs')
 
-//QRcode.toFile("./QR/000001.png","000001")
-
-exec("node_modules\\.bin\\http-server --cors")
-console.log("iniciado server en localhost:8080")
-
-// var HTML
 var etiquetas
 var longitud
 
@@ -17,20 +11,6 @@ longitud = Object.keys(etiquetas).length/4
 for (var i = 0;i<longitud;i++){
   qrcode.toFile("QR/" + etiquetas[i + '-0'],etiquetas[i + '-1'])
 }
-// fs.readFile('./etiquetas.json', (err, html) => {
-//     if(err){
-//         throw err;
-//     } else ETIQUETAS= etiquetas
-// });
-//
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-type', 'text/html');
-//     res.write(HTML)
-//     res.write(ETIQUETAS)
-//     res.end();
-// });
-//
-// server.listen(port, hostname, () => {
-//     console.log('Server started on port ' + port);
-// });
+
+exec("node_modules\\.bin\\http-server --cors")
+console.log("iniciado server en localhost:8080")
